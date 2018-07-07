40d5a55a839ea9190b1fe242752b022c11847820
uzmykai@gmail.com <uzmykai@gmail.com>
2018-07-07 22:17:03 +0100

    Add project files.

---
+++ UZ_PAY/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UZ_PAY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
